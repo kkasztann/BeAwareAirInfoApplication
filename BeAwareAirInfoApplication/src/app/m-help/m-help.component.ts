d88821b55d401a42ce8bf86aec8534daa2e0f40a
nulizmatyk <motreba1996@gmail.com>
2018-04-26 20:19:40 +0200

    Podpięte głowne opcje menu

--- BeAwareAirInfoApplication/src/app/m-help/m-help.component.ts
+++ BeAwareAirInfoApplication/src/app/m-help/m-help.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MHelpComponent implements OnInit {
 
+  visibleAbout = false;
+  visibleSupport = false;
+  visibleRate = true;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+   toggleAbout() {
+    this.visibleAbout = true;
+    this.visibleSupport = false;
+    this.visibleRate = false;
+  }
+  toggleSupport() {
+    this.visibleAbout = false;
+    this.visibleSupport = true;
+    this.visibleRate = false;
+  }
+  toggleRate() {
+    this.visibleAbout = false;
+    this.visibleSupport = false;
+    this.visibleRate = true;
   }
 
 }
