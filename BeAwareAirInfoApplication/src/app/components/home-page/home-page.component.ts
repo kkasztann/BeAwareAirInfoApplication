96e860a647517dac1ddccf40431a2494afefb366
Daniel Kasztan Słowik <kkasztann@gmail.com>
2018-05-11 18:03:15 +0200

    poprawki strony głównej, oraz dodanie brakujących marginesów

--- BeAwareAirInfoApplication/src/app/components/home-page/home-page.component.ts
+++ BeAwareAirInfoApplication/src/app/components/home-page/home-page.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
+  // tslint:disable-next-line:max-line-length
+  home1 = 'https://firebasestorage.googleapis.com/v0/b/beawareairinfoapplication.appspot.com/o/home1.png?alt=media&token=74e6a7ea-cd17-4cfe-b74c-03be05165365';
+  // tslint:disable-next-line:max-line-length
+  home2 = 'https://firebasestorage.googleapis.com/v0/b/beawareairinfoapplication.appspot.com/o/home2.png?alt=media&token=aa0218b1-f5b6-4618-8daf-2e6ac51fdad3';
+
   constructor() { }
 
   ngOnInit() {
